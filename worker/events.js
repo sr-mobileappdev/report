693bed181c3215a5dbedac01b643cdede107be34
Bilal Saeed <bilalsaeedhb@gmail.com>
2019-06-30 21:50:07 +0500

    dev-filter - fixed styling issues

--- worker/events.js
+++ worker/events.js
@@ -1,14 +1,13 @@
 'use strict'
 
 module.exports = (stream) => {
-
-  const logsArray = stream.events;
-  let count;
+  const logsArray = stream.events
+  let count
   for (let i = 0; i < logsArray.length; i++) {
-    if (logsArray[i].message.includes("count:")) {
-      count = logsArray[i].message.substring(7).trim();
-      console.log('Master Log count......', count);
+    if (logsArray[i].message.includes('count:')) {
+      count = logsArray[i].message.substring(7).trim()
+      console.log('Master Log count......', count)
     }
   }
-  return count;
+  return count
 }
--- worker/loop.js
+++ worker/loop.js
@@ -2,32 +2,26 @@
 
 module.exports = async (awsLogFunc, params, nextTokenName, processResults) => {
   try {
-    let finalResults, resultCount;
+    let finalResults
 
     // loop over log streams for the log group OR log events for the log stream
     do {
-      const awsLogFuncResult = await awsLogFunc(params)      
+      const awsLogFuncResult = await awsLogFunc(params)
       const nextToken = awsLogFuncResult[nextTokenName]
 
       const nextTokenIsUnique = Boolean(
-        nextToken
-        &&
+        nextToken &&
         (
-          nextTokenName === 'nextToken'
-          ||
+          nextTokenName === 'nextToken' ||
           (nextTokenName === 'nextForwardToken' && params[nextTokenName])
-        )
-        &&
+        ) &&
         !(nextToken === params[nextTokenName])
       )
       finalResults = await processResults(awsLogFuncResult)
 
-      if(nextTokenIsUnique)
-        params[nextTokenName] = nextToken
-      else
-        delete params[nextTokenName]
+      if (nextTokenIsUnique) { params[nextTokenName] = nextToken } else { delete params[nextTokenName] }
     } while (params[nextTokenName])
-    return finalResults;
+    return finalResults
   } catch (exception) {
     throw new Error(`\n[worker] loop\n${exception}\n`)
   }
