e89cb9ba2a4658d3805fddf8e60fbcf9459370b4
gentle-man85 <carreno8564@gmail.com>
2019-07-07 14:41:10 -0400

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -20,6 +20,10 @@ const logGroupName = `${bjcType}${eId}`;// THIS SEMI-COLON IS REQUIRED
   console.log(`finalResults ${logGroupName}`)
   console.log(JSON.stringify(finalResults, null, 1))
   console.log('BJC', finalResults.numbBjcStreams, finalResults.resultCount)
-  if (parseInt(finalResults.numbBjcStreams) === parseInt(finalResults.resultCount)) { console.log('Matching criteria Success!!!!!!') } else { console.log('Matching criteria Failure #####') }
+  if (parseInt(finalResults.numbBjcStreams) === parseInt(finalResults.resultCount)) {
+    console.log('Matching criteria Success!!!!!!')
+  } else {
+    console.log('Matching criteria Failure #####')
+  }
   console.log()
 })()
